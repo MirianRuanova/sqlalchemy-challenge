0903fe6d1104992780f9cb13a25dd5a75aca0656
MirianRuanova <mruanova.us@gmail.com>
2023-05-19 20:37:47 -0700

    Part 1: Analyze and Explore the Climate Data

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,21 @@
 # Import the dependencies.
+from sqlalchemy import create_engine
+#from sqlalchemy import Column, Integer, String, Float
 
+#from sqlalchemy.ext.declarative import declarative_base
+#Base = declarative_base()
 
+# Path to sqlite
+
+database_path = "../Resources/hawaii.sqlite"
+
+# Create an engine that can talk to the database
+engine = create_engine(f"sqlite:///{database_path}")
+# Query All Records in the the Database
+data = engine.execute("SELECT * FROM hawaii")
+
+for record in data:
+    print(record)
 
 #################################################
 # Database Setup
