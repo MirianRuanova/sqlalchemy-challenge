9c14b0a78d8ebecd33d3f651de83076f5ea63523
MirianRuanova <mruanova.us@gmail.com>
2023-07-05 22:40:22 -0700

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,43 @@
 
 #### Congratulations! You've decided to treat yourself to a long holiday vacation in Honolulu, Hawaii. To help with your trip planning, you decide to do a climate analysis about the area. The following sections outline the steps that you need to take to accomplish this task.
 
+## Reflect Tables into SQLAlchemy ORM
+
+1. Create our `engine` to connect with the database file `hawaii.sqlite`. The engine is a component that povides the interface to a database. It is reponsible fot establishing and managing the connections to the database and executing SQL queries.
+2. Define a Base class that will serve as the base for the generated classes representing database tables. Create an instance of it 
+
+    ```python
+    Base = automap_base()
+    ```
+3. Use `prepare()` method of `Base` class to reflect the schema of the existing database. Provide `engine` object as an argument. 
+    ```python
+    Base.prepare(engine, reflect=True)
+     ```
+4. To display all the classes that automap found, access the `classes` atribute of `Base` object.
+    ```python
+    classes = Base.classes
+    classes_name = Base.classes.keys()
+    print(classes_name)
+     ```
+    The `Base.classes` provides an dictionary-like interface that contains all the classes taht were automatically mapped. The `keys()` method is used to retrieve the names of the classes.
+5. To save references to each table after reflecting the database schema using automap_base in SQLAlchemy, you can store the generated classes in variables.
+    ```python
+    measurement = Base.classes.measurement
+    station = Base.classes.station
+     ```
+    The variables can be used to query and manipulate data from the respective tables.
+6. To create a session in SQLAlchemy and establish a connection between Python and the database use the `Session()` class from `sqlalchemy.orm`, passing the engine as an argument. The `Session` object will serve as the entry point for all database operations.
+
+## Exploratory Precipitation Analysis
+
+### Find the most recent date in the data set
+
+#### In the code below, ``func.max(measurement.date)`` is used within the ``query()`` method to find the maximum (most recent) date in the ``"date" `` column of the ``"measurement"`` table. The ``scalar()`` method is then called to retrieve the result as a single value.
+
+### Design a query to retrieve the last 12 months of precipitation data.
+
+## Part 2: Design Your Climate App
+
+#### After completing the initial analysis, we will design a Flask API based on the queries that we just developed. To do so, we will use Flask to create our routes as follows:
+
+Flask is a webframework for building web applications in Python. It provides a simple
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,45 +1,103 @@
 # Import the dependencies.
-from sqlalchemy import create_engine
-#from sqlalchemy import Column, Integer, String, Float
-
-#from sqlalchemy.ext.declarative import declarative_base
-#Base = declarative_base()
-
-# Path to sqlite
-
-database_path = "../Resources/hawaii.sqlite"
-
-# Create an engine that can talk to the database
-engine = create_engine(f"sqlite:///{database_path}")
-# Query All Records in the the Database
-data = engine.execute("SELECT * FROM hawaii")
-
-for record in data:
-    print(record)
-
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+    
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
+
+Base.prepare(engine, reflect = True)
 
 # reflect the tables
-
+classes = Base.classes
+classes_name = Base.classes.keys()
+print(classes_name)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
+from flask import Flask, jsonify
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route('/')
+def home():
+    """Homepage."""
+    return (
+        f"Welcome to the Hawaii Climate API!<br/><br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation - Precipitation data for the last 12 months<br/>"
+        f"/api/v1.0/stations - List of stations<br/>"
+        f"/api/v1.0/tobs - Temperature observations for the last 12 months<br/>"
+        f"/api/v1.0/&lt;start&gt; - Min, Avg, and Max temperatures from a start date (format: 'yyyy-mm-dd')<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt; - Min, Avg, and Max temperatures within a date range (format: 'yyyy-mm-dd')<br/>"
+    )
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    """Return the JSON representation of the last 12 months of precipitation data."""
+    # Calculate the date one year ago from the most recent date in the database
+    one_year_ago = dt.date.today() - dt.timedelta(days=365)
+
+    # Perform the query to retrieve the last 12 months of precipitation data
+    results = session.query(measurement.date, measurement.prcp).\
+              filter(measurement.date >= one_year_ago).all()
+
+    # Convert the query results to a dictionary
+    precipitation_data = {date: prcp for date, prcp in results}
+
+    return jsonify(precipitation_data)
+
+@app.route('/api/v1.0/stations')
+def stations():
+    """Return a JSON list of stations from the dataset."""
+    # Perform the query to retrieve all stations
+    results = session.query(station.station, station.name).all()
+
+    # Convert the query results to a list of dictionaries
+    station_list = []
+    for station, name in results:
+        station_list.append({
+            'station': station,
+            'name': name
+        })
+
+    return jsonify(station_list)
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+    """Return a JSON list of temperature observations for the previous year for the most active station."""
+    # Calculate the date one year ago from the most recent date in the database
+    one_year_ago = dt.date.today() - dt.timedelta(days=365)
+
+    # Perform the query to retrieve the temperature observations for the most active station
+    results = session.query(measurement.date, measurement.tobs).\
+              filter(measurement.station == 'USC00519281').\
+              filter(measurement.date >= one_year_ago).all()
+
+
+    
+    
